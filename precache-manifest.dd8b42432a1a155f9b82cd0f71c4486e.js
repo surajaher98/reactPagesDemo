f8b7f6e2ed818ee0570264008f68875a0d717eec
Suraj Aher <suraj.aher@nitorinfotech.com>
2020-08-23 13:08:08 +0530

    Updates

---
+++ precache-manifest.dd8b42432a1a155f9b82cd0f71c4486e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c5a0a1767f8f4fa244700faa2a1b0a",
+    "url": "/reactPagesDemo/index.html"
+  },
+  {
+    "revision": "c3ebae8e1d65b8a41bcd",
+    "url": "/reactPagesDemo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/reactPagesDemo/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPagesDemo/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ebae8e1d65b8a41bcd",
+    "url": "/reactPagesDemo/static/js/main.c066379e.chunk.js"
+  },
+  {
+    "revision": "6f877537200274ea51c3",
+    "url": "/reactPagesDemo/static/js/runtime-main.2a4e94a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactPagesDemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
